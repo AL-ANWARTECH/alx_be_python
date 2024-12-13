e2bad3398fa22dcb0aff9114618dad884f100194
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2024-12-13 15:15:50 -0800

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+# Calculate The Simple interest
+principal = 1000
+rate = 0.05
+time = 3
+simple_interest = principal * rate * time
+#Print Simple interest
+print(f"The simple interest is: {simple_interest}")
